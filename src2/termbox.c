12f94d4bc27b0f124c4685cbf173038246af359c
radicalten <79894181+radicalten@users.noreply.github.com>
2025-09-27 23:18:31 -0500

    Update termbox.c

--- src2/termbox.c
+++ src2/termbox.c
@@ -60,7 +60,7 @@ SOFTWARE.
 #include <termios.h>
 #include <unistd.h>
 #include <wchar.h>
-#include "termbox2.h"
+#include "termbox2.h" // updated to termbox2.h
 
 #ifndef IMAXBEL
 #define IMAXBEL 0
